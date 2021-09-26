57878012676e455c6e5fe04c85c2f02f5f8a0d8e
chekai7 <rockkcor75@gmail.com>
2021-09-26 15:14:24 +0800

    Add Dynamic import code

--- containers/us/Home/Home.js
+++ containers/us/Home/Home.js
@@ -1,21 +1,50 @@
-import React from 'react';
+import React,{Suspense} from 'react';
+import dynamic from 'next/dynamic';
 import KeyVisual from '@/components/pages/Home/KeyVisual/KeyVisual';
-import Intro from '@/components/pages/Home/Introduction/Introduction';
-import Faas from '@/components/pages/Home/Faas/Faas';
-import HomeSmartAlert from './HomeSmartAlert';
-import CloudRecording from '@/components/pages/Home/CloudRecording';
-import DesignedForDogs from '@/components/pages/Home/DesignedForDogs';
+// import Intro from '@/components/pages/Home/Introduction/Introduction';
+// import Faas from '@/components/pages/Home/Faas/Faas';
+// import HomeSmartAlert from './HomeSmartAlert';
+// import CloudRecording from '@/components/pages/Home/CloudRecording';
+// import DesignedForDogs from '@/components/pages/Home/DesignedForDogs';
 
 const Home = () => {
   // React 18 suspense improve tti tbt ?
+
+ 
+  const DynamicLazyIntroComponent = dynamic(() => import(/* webpackChunkName: "Introduction" */'@/components/pages/Home/Introduction/Introduction'), {
+    suspense: true,
+  })
+  const DynamicLazyFaasComponent = dynamic(() => import(/* webpackChunkName: "Faas" */'@/components/pages/Home/Faas/Faas'), {
+    suspense: true,
+  })
+  const DynamicLazyHomeSmartAlertComponent = dynamic(() => import(/* webpackChunkName: "HomeSmartAlert" */'./HomeSmartAlert'), {
+    suspense: true,
+  })
+  const DynamicLazyCloudRecordingComponent = dynamic(() => import(/* webpackChunkName: "CloudRecording" */'@/components/pages/Home/CloudRecording'), {
+    suspense: true,
+  })
+ const DynamicLazyDesignedForDogsComponent = dynamic(() => import(/* webpackChunkName: "DesignedForDogs" */'@/components/pages/Home/DesignedForDogs'), {
+    suspense: true,
+  })
+
   return (
     <>
       <KeyVisual />
-      <Intro />
-      <Faas />
-      <HomeSmartAlert />
-      <CloudRecording />
-      <DesignedForDogs />
+      <Suspense fallback={`loading`}>
+        <DynamicLazyIntroComponent />
+      </Suspense>
+      <Suspense fallback={`loading`}>
+        <DynamicLazyFaasComponent />
+      </Suspense>
+      <Suspense fallback={`loading`}>
+        <DynamicLazyHomeSmartAlertComponent />
+      </Suspense>
+      <Suspense fallback={`loading`}>
+        <DynamicLazyCloudRecordingComponent />
+      </Suspense>
+      <Suspense fallback={`loading`}>
+        <DynamicLazyDesignedForDogsComponent />
+      </Suspense>
     </>
   )
 }
