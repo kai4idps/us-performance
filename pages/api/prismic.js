66e1ca55c1c24022f9c42e01ff285ad971710383
chekai7 <rockkcor75@gmail.com>
2021-09-30 13:34:15 +0800

    Fix api

--- pages/api/prismic.js
+++ pages/api/prismic.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
 
   res.status(200).json({ 
     key_visual_image_mobile: {
-      url: 'https://images.prismic.io/}furbo-prismic/35b96df6-ffc6-4999-8087-d7a51c285c19_homepage-kv-mobile.jpg?auto=compress,format&rect=0,20,345,460&w=375&h=500'
+      url: 'https://images.prismic.io/furbo-prismic/35b96df6-ffc6-4999-8087-d7a51c285c19_homepage-kv-mobile.jpg?auto=compress,format&rect=0,20,345,460&w=375&h=500'
     }, 
     key_visual_image_desktop: {
       url :'https://images.prismic.io/furbo-prismic/918b4b06-fa9c-4bc1-b9c3-520c1a994e80_homepage-kv-desktop.jpg?auto=compress,format&rect=0,23,1180,553&w=1280&h=600'
