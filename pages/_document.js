1eadbfc436d01adad3d47d7405e667f2dce46dd4
chekai7 <rockkcor75@gmail.com>
2021-10-06 14:29:46 +0800

    Fix documenr

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheets } from "@material-ui/core/styles";
-import { HOME_PHONE_VIEW_WEBP } from "@/config/imageConfigs/home";
+// import { HOME_PHONE_VIEW_WEBP } from "@/config/imageConfigs/home";
 
 export default class MyDocument extends Document {
   render() {
