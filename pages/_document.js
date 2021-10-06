4dfe8dc1eeca25aa525fafa2573b3bb8168f5ebc
chekai7 <rockkcor75@gmail.com>
2021-10-06 14:29:22 +0800

    Fix documenr

--- pages/_document.js
+++ pages/_document.js
@@ -1,82 +1,80 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheets } from "@material-ui/core/styles";
-import {
-    HOME_PHONE_VIEW_WEBP,
-} from '@/config/imageConfigs/home';
+import { HOME_PHONE_VIEW_WEBP } from "@/config/imageConfigs/home";
 
 export default class MyDocument extends Document {
-	render() {
-		return (
-			<Html lang="en">
-				<Head>
-					<link
-						href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
-						rel="stylesheet"
-					/>
-					  {/* <link
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+            rel="stylesheet"
+          />
+          {/* <link
 							rel="preload"
 							href={HOME_PHONE_VIEW_WEBP}
 							as="image"
 						/> */}
-					  <link
+          {/* <link
 							rel="prefetch"
 							href={'https://images.prismic.io/furbo-prismic/35b96df6-ffc6-4999-8087-d7a51c285c19_homepage-kv-mobile.jpg?auto=compress,format&rect=0,20,345,460&w=375&h=500'}
 							as="image"
-						/>
-				</Head>
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		);
-	}
+						/> */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 // `getInitialProps` belongs to `_document` (instead of `_app`),
 // it's compatible with server-side generation (SSG).
-MyDocument.getInitialProps = async ctx => {
-	// Resolution order
-	//
-	// On the server:
-	// 1. app.getInitialProps
-	// 2. page.getInitialProps
-	// 3. document.getInitialProps
-	// 4. app.render
-	// 5. page.render
-	// 6. document.render
-	//
-	// On the server with error:
-	// 1. document.getInitialProps
-	// 2. app.render
-	// 3. page.render
-	// 4. document.render
-	//
-	// On the client
-	// 1. app.getInitialProps
-	// 2. page.getInitialProps
-	// 3. app.render
-	// 4. page.render
+MyDocument.getInitialProps = async (ctx) => {
+  // Resolution order
+  //
+  // On the server:
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. document.getInitialProps
+  // 4. app.render
+  // 5. page.render
+  // 6. document.render
+  //
+  // On the server with error:
+  // 1. document.getInitialProps
+  // 2. app.render
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the client
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. app.render
+  // 4. page.render
 
-	// Render app and page and get the context of the page with collected side effects.
-	const sheets = new ServerStyleSheets();
-	const originalRenderPage = ctx.renderPage;
+  // Render app and page and get the context of the page with collected side effects.
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
 
-	ctx.renderPage = () =>
-		originalRenderPage({
-			// eslint-disable-next-line react/display-name
-			enhanceApp: App => props => sheets.collect(<App {...props} />)
-		});
+  ctx.renderPage = () =>
+    originalRenderPage({
+      // eslint-disable-next-line react/display-name
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
 
-	const initialProps = await Document.getInitialProps(ctx);
+  const initialProps = await Document.getInitialProps(ctx);
 
-	return {
-		...initialProps,
-		// Styles fragment is rendered after the app and page rendering finish.
-		styles: [
-			...React.Children.toArray(initialProps.styles),
-			sheets.getStyleElement()
-		]
-	};
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
 };
